61d03ae221e6fe150115d6d8cde832a50ea9a0f7
jaehyeonjung0613 <jaehyeonjung0613@gmail.com>
2025-01-09 14:24:02 +0900

    feat: 9. 사용자 입력 처리 - [Game.java] 애플리케이션 실행시 자동차 이름과 시도 횟수 입력 - [GameConstants.java] Game 상수 클래스 생성, 사용자 입력 관련 상수 정의

--- src/main/java/racingcar/service/Game.java
+++ src/main/java/racingcar/service/Game.java
@@ -1,5 +1,14 @@
 package racingcar.service;
 
+import static racingcar.service.GameConstants.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.swing.*;
+
+import racingcar.entity.Car;
 import racingcar.ui.Input;
 import racingcar.ui.Output;
 
@@ -22,6 +31,24 @@ public class Game {
     }
 
     public void run() {
+        List<Car> carList = this.createCarList(this.questInputCarName());
+        int finalRound = this.questInputFinalRound();
+    }
+
+    private String[] questInputCarName() {
+        this.output.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String answer = this.input.readline();
+        return answer.split(CAR_NAME_SEPARATOR);
+    }
+
+    private List<Car> createCarList(String[] carNames) throws IllegalArgumentException {
+        return Arrays.stream(carNames).map(Car::new).collect(Collectors.toList());
+    }
+
+    private int questInputFinalRound() {
+        this.output.println("시도할 회수는 몇회인가요?");
+        String answer = this.input.readline();
+        return Integer.parseInt(answer);
     }
 
     public void finish() {
---
+++ src/main/java/racingcar/service/GameConstants.java
@@ -0,0 +1,8 @@
+package racingcar.service;
+
+public final class GameConstants {
+    private GameConstants() {
+    }
+
+    public static final String CAR_NAME_SEPARATOR = ",";
+}
